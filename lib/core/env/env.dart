67bc1f902807f6c5615f96ac5a7d301e960c9885
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2024-02-23 06:04:34 +0700

    Add MAPBOX_ACCESS_TOKEN and STYLE_ID to environment variables

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -10,4 +10,8 @@ abstract class Env {
   static const String layerId = _Env.layerId;
   @EnviedField(varName: 'PROJECT_ID')
   static const String projectId = _Env.projectId;
+  @EnviedField(varName: 'MAPBOX_ACCESS_TOKEN')
+  static const String accessToken = _Env.accessToken;
+  @EnviedField(varName: 'STYLE_ID')
+  static const String styleId = _Env.styleId;
 }
